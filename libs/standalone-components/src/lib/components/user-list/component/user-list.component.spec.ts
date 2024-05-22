d6a6233d2357dacc9f82a1d344940bb0439c32b1
Jakub Szmidt <kubas3172@gmail.com>
2024-05-22 15:12:10 +0200

    Add tests to user list component

--- libs/standalone-components/src/lib/components/user-list/component/user-list.component.spec.ts
+++ libs/standalone-components/src/lib/components/user-list/component/user-list.component.spec.ts
@@ -1,18 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
 import { of } from 'rxjs';
+import { DebugElement } from '@angular/core';
+import { IUser } from '@demo-testing-app/shared';
+import { By } from '@angular/platform-browser';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserListComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(UserListComponent);
+    fixture = TestBed.overrideComponent(UserListComponent, {
+      set: { host: { '(click)': 'dummy' } },
+    }).createComponent(UserListComponent);
+
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
 
     component.props = {
       users$: of([
@@ -23,8 +31,8 @@ describe('UserListComponent', () => {
           email: 'email@email.com',
         },
       ]),
-      onAddClick: () => {},
-      onRemoveClick: () => {},
+      onAddClick: (): void => {},
+      onRemoveClick: (): void => {},
     };
 
     fixture.detectChanges();
@@ -32,5 +40,51 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display list of users', () => {
+    const users: IUser[] = [
+      {
+        id: 1,
+        name: 'name',
+        lastName: 'last name',
+        email: 'email@email.com',
+      },
+      {
+        id: 2,
+        name: 'name2',
+        lastName: 'last name2',
+        email: 'email2@email.com',
+      },
+    ];
+
+    component.props.users$ = of(users);
+    debugElement.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const userComponents: HTMLElement[] = debugElement
+      .queryAll(By.css('lib-user-container'))
+      .map((debugEl) => debugEl.nativeElement);
+
+    expect(userComponents.length).toBe(2);
+    userComponents.forEach((element) => {
+      expect(element).toBeVisible();
+    });
+  });
+
+  it('should display Add Random User button', () => {
+    const button: HTMLElement = debugElement.query(
+      By.css('#add-random-user-button'),
+    ).nativeElement;
+
+    expect(button).toBeVisible();
+  });
+
+  it('should display Remove Last User button', () => {
+    const button: HTMLElement = debugElement.query(
+      By.css('#remove-last-user-button'),
+    ).nativeElement;
+
+    expect(button).toBeVisible();
   });
 });
