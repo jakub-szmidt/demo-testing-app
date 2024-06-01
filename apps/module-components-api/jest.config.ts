1253762e8c76f37ad752a3fb6b5f152d2c95ee96
Jakub Szmidt <kubas3172@gmail.com>
2024-06-01 19:23:13 +0200

    Add node apps

---
+++ apps/module-components-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'module-components-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/module-components-api',
+};
